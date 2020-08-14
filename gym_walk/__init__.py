0da9f2353738a887fe59021b139954f1cc8f9c8e
ercosv <36733999+ercosv@users.noreply.github.com>
2020-08-14 12:53:33 +0900

    Update __init__.py

--- gym_walk/__init__.py
+++ gym_walk/__init__.py
@@ -165,6 +165,15 @@ register(
     nondeterministic=True,
 )
 register(
+    id='Investment-v0',
+    entry_point='gym_walk.envs:WalkEnv',
+    # left-most and right-most states are terminal
+    kwargs={'n_states': 7, 'p_stay': 0.5*2/3., 'p_backward': 0.5*1/3.},
+    max_episode_steps=100,
+    reward_threshold=1.0,
+    nondeterministic=True,
+)
+register(
     id='SlipperyWalkFifthteen-v0',
     entry_point='gym_walk.envs:WalkEnv',
     # left-most and right-most states are terminal
