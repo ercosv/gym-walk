299cc5847c7a54ccf10b449cec1a9cda639ebf79
ercosv <36733999+ercosv@users.noreply.github.com>
2020-08-14 12:52:41 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ setup(
     version='0.0.1',
     description='Gym walk environment - useful to replicate Random Walk experiments',
     url='https://github.com/ercosv/gym-walk',
-    author='Miguel Morales',
-    author_email='mimoralea@gmail.com',
     packages=['gym_walk', 'gym_walk.envs'],
-    license='MIT License',
     install_requires=['gym'],
 )
